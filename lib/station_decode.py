018e48dac6199a12de3c15dbbb589ac3cddef5ff
petesndrs <petesndrs@users.noreply.github.com>
2018-04-08 17:35:27 +0100

    Fix HTMLParser handling of &amp; escaped ampersands

--- lib/station_decode.py
+++ lib/station_decode.py
@@ -10,6 +10,7 @@ class StationDecode(HTMLParser):
     td_count = 0
     station = {}
     station_list = []
+    add_amp = False
 
     def open(self, filename):
         '''Method open
@@ -40,9 +41,12 @@ class StationDecode(HTMLParser):
     def handle_data(self, data):
         '''Method handle_data
         '''
-        # print("Encountered some data  :", data)
-        if self.td_count == 1 and 'name' not in self.station:
-            self.station['name'] = data
+        # print('Data  @ td {}:{}'.format(self.td_count,data))
+        if self.td_count == 1:
+            if self.add_amp:
+                self.station['name'] += '&{}'.format(data)
+            else:
+                self.station['name'] = data
         elif self.td_count == 7:
             try:
                 self.station['lo'] = float(data)
@@ -55,6 +59,15 @@ class StationDecode(HTMLParser):
                 self.station_list.append(self.station.copy())
             except ValueError:
                 print("WARNING latitude not float " + self.station['name'])
+
+        if self.add_amp:
+            self.add_amp = False
+
+    def handle_entityref(self, name):
+        '''Method handle_entityref
+        '''
+        if name == 'amp':
+            self.add_amp = True
 
     def get_stations(self):
         '''Method get_stations
